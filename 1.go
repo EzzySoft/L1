3200a1292a13c3ecba4d9906b822d3717d6191a5
EzzySoft <daniyatretyakov@gmail.com>
2025-07-21 21:11:10 +0300

    feat: solve task 1

---
+++ 1.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Структура Human
+type Human struct {
+	Name string
+	Age  int
+}
+
+// Метод Human
+func (h *Human) SayHello() {
+	fmt.Printf("Привет, меня зовут %s!\n", h.Name)
+}
+
+// Встраивание Human в Action (композиция)
+type Action struct {
+	Human
+	Type string
+}
+
+// Метод только для Action
+func (a *Action) DoSomething() {
+	fmt.Println("Выполняю действие:", a.Type)
+}
+
+func main() {
+	// Создаём экземпляр Action
+	a := Action{
+		Human: Human{Name: "Данила", Age: 21},
+		Type:  "Учить Golang",
+	}
+	// Вызываем метод Human через Action
+	a.SayHello()
+	// Вызываем метод Action
+	a.DoSomething()
+}
